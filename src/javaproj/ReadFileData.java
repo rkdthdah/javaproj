6348f52b94f13f2a78f375ded64579162a96cf70
rkdthdah <43609781+rkdthdah@users.noreply.github.com>
2019-05-20 16:44:20 +0900

    #1

---
+++ src/javaproj/ReadFileData.java
@@ -0,0 +1,26 @@
+package javaproj;
+import java.io.*;
+
+public class ReadFileData {
+
+	public static void main(String[] args) {
+		int b = 0;
+		StringBuffer buffer = new StringBuffer();
+		FileInputStream file = null;
+		try {
+			file = new FileInputStream("Stack.h");
+			b = file.read();
+			while (b != -1) {
+				buffer.append((char)b);
+				b = file.read();
+			}
+			System.out.println(buffer);
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("Oops: FileNotFoundException");
+		}
+		catch (IOException e) {
+			System.out.println("Input error");
+		}
+	}
+}
