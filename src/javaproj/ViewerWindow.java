9cb08024d46cdea8091d43953d82d57c925938e1
seungri0826 <seungri0826@naver.com>
2019-05-26 19:41:59 +0900

    #5
    ViewerWindow

--- src/javaproj/ViewerWindow.java
+++ src/javaproj/ViewerWindow.java
@@ -5,13 +5,21 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class ViewerWindow extends JFrame {
-	JMenuBar menuBar;
-	JMenu menu;
-	JMenuItem open, save, exit;
 	
-	public ViewerWindow() {
+	
+	ViewerWindow() {
 		setTitle("C++ Å¬·¡½º Viewer");
 		setSize(1200,800);
+		createMenu();
+		
+		setVisible(true);
+	}
+	
+	void createMenu() {
+		JMenuBar menuBar;
+		JMenu menu;
+		JMenuItem open, save, exit;
+		
 		menuBar = new JMenuBar();
 		menu = new JMenu("File");
 		open = new JMenuItem("Open");
@@ -21,12 +29,20 @@ public class ViewerWindow extends JFrame {
 		menu.add(save);
 		menu.addSeparator();
 		menu.add(exit);
+		exit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Object source = e.getSource();
+				if (source == exit) {
+					System.exit(0);
+				}
+			}
+		});
 		menuBar.add(menu);
 		
 		setJMenuBar(menuBar);
-		setVisible(true);
-	}
 	
+	
+	}
 	
 	
 
