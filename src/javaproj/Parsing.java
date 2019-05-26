db8435c84167f5444896a90d96ab52775456c923
rkdthdah <43609781+rkdthdah@users.noreply.github.com>
2019-05-26 15:49:40 +0900

    #2
    클래스파싱함수

--- src/javaproj/Parsing.java
+++ src/javaproj/Parsing.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class Parsing {
 	private Token t;
 	private ArrayList<String> line;
+	private int bracecnt = -1;
+	private String access;
 	
 	Parsing(){
 		t = new Token();
@@ -15,12 +17,14 @@ public class Parsing {
 		while(line != null) {
 			BringToken();
 			
-			ClassInfo arrayofclass[];
+			ClassInfo classinfo[] = new ClassInfo[5];
 			int classcnt = 0;
 			int loc = FindWord("class");
 			if (loc != -1) {
-				arrayofclass[classcnt] = new ClassInfo(line.get(loc + 1));
+				classinfo[classcnt] = new ClassInfo(line.get(loc + 1));
+				classcnt += 1;
 				ClassParsing();
+				continue;
 			}
 			
 		}
@@ -31,14 +35,18 @@ public class Parsing {
 		if (line != null) {
 			line = t.Tokenizer();
 		}
+		System.out.println(line);
 	}
 	
 	int FindWord(String w) {
+		System.out.println("Find " + w);
 		int loc = -1;
 		while (true) {
 			try {
-				if (line.get(loc + 1).matches(".*"+ w + ".*")) {
+				String findstr = ".*" + w + ".*";
+				if (line.get(loc + 1).matches(findstr)) {
 					loc += 1;
+					System.out.println(w +" "+ loc);
 					return loc;
 				}
 			}
@@ -54,8 +62,69 @@ public class Parsing {
 	}
 	
 	void ClassParsing () {
-		BringToken();
-		int bracecnt = -1;
+		System.out.println("cp");
+		int methodopen = 0;
+		while (true) {
+			if (FindWord("\\{") != -1) {
+				bracecnt += 1;
+				System.out.println(bracecnt);
+				if (bracecnt >= 1) MethodParsing();
+			}
+			
+			int accessloc = FindWord(":");
+			if (accessloc != -1) {
+				access = line.get(accessloc).split(":").toString();
+				System.out.println(access);
+			}
+			
+			methodopen = FindWord("\\(");
+			String methodname;
+			String methodtype;
+			String factor = null;
+			
+			int methodcnt = 0;
+			MethodInfo arrayofmethod[] = new MethodInfo[10];
+			
+			if (methodopen != -1) {
+				methodname = line.get(methodopen).split("\\(").toString();
+				if (methodname.equals("")) {
+					methodopen -= 1;
+					methodname = line.get(methodopen);			
+				}
+				
+				try {
+					methodtype = line.get(methodopen - 1);
+				}
+				catch (IndexOutOfBoundsException e) {
+					methodtype = "void";
+				}
+				if (!(methodtype.equals("void")||methodtype.equals("bool")||methodtype.equals("int"))) {
+					methodtype = "void";
+				}
+				
+				int methodclose = FindWord("\\)");
+				int factorloc = FindWord("int");
+				System.out.println(methodopen +" "+ methodclose);
+				if ((methodopen <= factorloc)||(factorloc <= methodclose)) {
+					factor = line.get(factorloc);
+				}
+				arrayofmethod[methodcnt] = new MethodInfo(methodname, methodtype, access);
+				arrayofmethod[methodcnt].setFactor(factor);
+				methodcnt += 1;
+				System.out.println(arrayofmethod[0].name);
+			}
+			if (FindWord("\\}") != -1) {
+				bracecnt -= 1;
+				if (bracecnt == -1) break;
+			}
+			BringToken();
+		}
 		
 	}
+
+		
+	void MethodParsing() {
+	}
+		
+		
 }
